8249cfdf4be694104dab33011665e377425b8843
LillySh <shakirovalilly@gmail.com>
2021-12-02 16:49:47 +0300

    New file to check how it works

---
+++ summator.py
@@ -0,0 +1,7 @@
+def summator(number1, number2):
+    print('Сумма ваших чисел равна =', number1 + number2)
+
+
+number1 = int(input('Введите первое число: '))
+number2 = int(input('Введите второе число: '))
+summator(number1, number2)
